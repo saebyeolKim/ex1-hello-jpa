9180743f9e9b3afe659dd62a096c762c4ddbb3af
Saebyeol <94090893+saebyeolKim@users.noreply.github.com>
2023-10-31 09:33:40 +0900

    값 타입의 비교

--- src/main/java/helloJpa/Address.java
+++ src/main/java/helloJpa/Address.java
@@ -1,6 +1,7 @@
 package helloJpa;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
@@ -38,5 +39,18 @@ public class Address {
 
     private void setStreet(String street) {
         this.street = street;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(zipcode, address.zipcode) && Objects.equals(street, address.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, zipcode, street);
     }
 }
