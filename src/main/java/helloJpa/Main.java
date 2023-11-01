1131bdf589d5ded8e6e9bb7e986a109831b5c85d
Saebyeol <94090893+saebyeolKim@users.noreply.github.com>
2023-11-01 09:22:43 +0900

    값 타입 컬렉션

--- src/main/java/helloJpa/Main.java
+++ src/main/java/helloJpa/Main.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,14 +19,33 @@ public class Main {
         tx.begin();     //트랜잭션 시작
         //code
         try {
-            Address address = new Address("city", "street", "1000");
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(address);
-            em.persist(member);
+            member.setHomeAddress(new Address("homeCity1", "street", "10000"));
 
-            Address newCity = new Address("NewCity", address.getZipcode(), address.getZipcode());
-            member.setHomeAddress(newCity);
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
+
+            em.persist(member);
+            
+            em.flush();
+            em.clear();
+
+            System.out.println("========== START ===========");
+            Member findMember = em.find(Member.class, member.getId());
+
+            //homeCity -> newCity
+//            findMember.getHomeAddress().setCity("newCity") 수정 시 이렇게 하면 안됨
+            Address a = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode())); //이렇게 객체를 새로 만들어서 통으로 바꿔야 함
+
+            //치킨 -> 한식
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
 
             tx.commit();    //커밋
         } catch (Exception e) {
